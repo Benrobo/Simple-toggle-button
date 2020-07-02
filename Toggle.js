9b0fefd128ac24a36dd2973c2ddd305b63a705ca
Alumona Benaiah <65370456+Benrobo@users.noreply.github.com>
2020-07-02 22:24:15 +0100

    Add files via upload

---
+++ Toggle.js
@@ -0,0 +1,15 @@
+
+function slide(){
+document.getElementById("btn").style.marginLeft = "45px";
+document.getElementById("togg").style.background = "green";
+
+document.getElementById("body").style.background = "black";
+document.getElementById("body").style.color = "white";
+}
+
+function glide(){
+document.getElementById("btn").style.marginLeft = "0";
+document.getElementById("togg").style.backgroundColor = "white";
+document.getElementById("body").style.background = "white";
+document.getElementById("header").style.color = "black";
+}
